8e8f5bf253b0bbaf45f35239fa5c13d9a967c9cc
skril349 <tvives.cabaleiro@gmail.com>
2023-09-19 10:16:56 +0200

    reading values

--- ODriveduino/ODriveduino.ino
+++ ODriveduino/ODriveduino.ino
@@ -18,15 +18,67 @@ int state = 0;
 int test;
 float move_to;
 float turns;
+
+HardwareSerial& odrive_serial = Serial1;
+
+// Arduino Mega or Due - Serial1
+// pin 19: RX - connect to ODrive TX
+// pin 18: TX - connect to ODrive RX
+// See https://www.arduino.cc/reference/en/language/functions/communication/serial/ for other options
+// HardwareSerial& odrive_serial = Serial1;
+
+
+
 // Arduino without spare serial ports (such as Arduino UNO) have to use software serial.
 // Note that this is implemented poorly and can lead to wrong data sent or read.
 // pin 8: RX - connect to ODrive TX
 // pin 9: TX - connect to ODrive RX
-SoftwareSerial odrive_serial(8, 9);
+
+//SoftwareSerial odrive_serial(8, 9);
 
 
 // ODrive object
 ODriveArduino odrive(odrive_serial);
+
+
+
+//functions
+float GetIntensity(int motor_number) {
+  odrive_serial << "r axis" << motor_number << ".motor.current_control.Iq_measured\n";
+
+  return readString().toFloat();
+}
+
+float GetVelocity(int motor_number) {
+  odrive_serial << "r axis" << motor_number << ".encoder.vel_estimate\n";
+
+  return readString().toFloat();
+}
+
+float GetPosition(int motor_number) {
+  odrive_serial << "r axis" << motor_number << ".encoder.pos_estimate\n";
+
+  return readString().toFloat();
+}
+
+
+String readString() {
+  String str = "";
+  static const unsigned long timeout = 1000;
+  unsigned long timeout_start = millis();
+  for (;;) {
+    while (!odrive_serial.available()) {
+      if (millis() - timeout_start >= timeout) {
+        return str;
+      }
+    }
+    char c = odrive_serial.read();
+    if (c == '\n')
+      break;
+    str += c;
+  }
+  return str;
+}
 
 void setup() {
   // ODrive uses 115200 baud
@@ -42,18 +94,18 @@ void setup() {
   // In this example we set the same parameters to both motors.
   // You can of course set them different if you want.
   // See the documentation or play around in odrivetool to see the available parameters
- odrive_serial << "w axis0.controller.config.vel_limit " << 100.0f << '\n';
-    odrive_serial << "w axis0.motor.config.current_lim " << 30.0f << '\n';
-    odrive_serial << "w axis0.trap_traj.config.accel_lim " << 50.0f << '\n';
-    odrive_serial << "w axis0.trap_traj.config.decel_lim " << 50.0f << '\n';
-    odrive_serial << "w axis0.trap_traj.config.vel_lim " << 100.0f << '\n';
+  odrive_serial << "w axis0.controller.config.vel_limit " << 30.0f << '\n';
+  odrive_serial << "w axis0.motor.config.current_lim " << 25.0f << '\n';
+  odrive_serial << "w axis0.trap_traj.config.accel_lim " << 10.0f << '\n';
+  odrive_serial << "w axis0.trap_traj.config.decel_lim " << 10.0f << '\n';
+  odrive_serial << "w axis0.trap_traj.config.vel_lim " << 30.0f << '\n';
 
-     odrive_serial << "w axis1.controller.config.vel_limit " << 100.0f << '\n';
-    odrive_serial << "w axis1.motor.config.current_lim " << 30.0f << '\n';
+  odrive_serial << "w axis1.controller.config.vel_limit " << 30.0f << '\n';
+  odrive_serial << "w axis1.motor.config.current_lim " << 25.0f << '\n';
 
-    odrive_serial << "w axis1.trap_traj.config.accel_lim " << 50.0f << '\n';
-    odrive_serial << "w axis1.trap_traj.config.decel_lim " << 50.0f << '\n';
-    odrive_serial << "w axis1.trap_traj.config.vel_lim " << 100.0f << '\n';
+  odrive_serial << "w axis1.trap_traj.config.accel_lim " << 10.0f << '\n';
+  odrive_serial << "w axis1.trap_traj.config.decel_lim " << 10.0f << '\n';
+  odrive_serial << "w axis1.trap_traj.config.vel_lim " << 30.0f << '\n';
 
   Serial.println("Ready!");
   Serial.println("calibration");
@@ -74,7 +126,7 @@ void setup() {
 
 
   int motornum2 = '1' - '0';
-requested_state = AXIS_STATE_MOTOR_CALIBRATION;
+  requested_state = AXIS_STATE_MOTOR_CALIBRATION;
   Serial << "Axis" << "1" << ": Requesting state " << requested_state << '\n';
   if (!odrive.run_state(motornum2, requested_state, true)) return;
 
@@ -85,9 +137,9 @@ requested_state = AXIS_STATE_MOTOR_CALIBRATION;
   requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL;
   Serial << "Axis" << "1" << ": Requesting state " << requested_state << '\n';
   if (!odrive.run_state(motornum2, requested_state, false /*don't wait*/)) return;
-  
 
-  Serial.println("presiona `t` si quieres hacer un ensayo de tracción y `r` si quieres uno de twist: ");
+
+  Serial.println("presiona `t` si quieres hacer un ensayo de tracción y `r` si quieres uno de twist & tracción: ");
 
 
 }
@@ -121,7 +173,7 @@ void loop() {
 
     case 2:
       if (test == 'r') {
-       Serial.println("Selecciona el numera de vueltas");
+        Serial.println("Selecciona el numera de vueltas");
         state = 3;
 
       }
@@ -130,12 +182,12 @@ void loop() {
         odrive_serial << "w axis0.controller.config.input_mode = INPUT_MODE_TRAP_TRAJ" << '\n';
         delay(5);
         odrive_serial << "t 0 " << move_to << '\n';
-        Serial.println("presiona 0 si quieres hacer un ensayo de tracción y 1 si quieres uno de twist: ");
+        Serial.println("presiona 't' si quieres hacer un ensayo de tracción y 'r' si quieres uno de twist & tracción: ");
         state = 0;
       }
       break;
-     case 3:
-      if (Serial.available()>0) {
+    case 3:
+      if (Serial.available() > 0) {
 
         String input = Serial.readStringUntil('\n'); // Lee la entrada hasta encontrar un salto de línea ('\n')
         // Intenta convertir la entrada a un número entero (int)
@@ -149,16 +201,41 @@ void loop() {
       }
       break;
 
-      case 4:
- Serial.println("moving trajectory");
-        odrive_serial << "w axis0.controller.config.input_mode = INPUT_MODE_TRAP_TRAJ" << '\n';
-        odrive_serial << "w axis1.controller.config.input_mode = INPUT_MODE_TRAP_TRAJ" << '\n';
-        delay(5);
-        odrive_serial << "t 0 " << move_to << '\n';
-        odrive_serial << "t 1 " << turns << '\n';
+    case 4:
+      Serial.println("moving trajectory");
+      odrive_serial << "w axis0.controller.config.input_mode = INPUT_MODE_TRAP_TRAJ" << '\n';
+      odrive_serial << "w axis1.controller.config.input_mode = INPUT_MODE_TRAP_TRAJ" << '\n';
+      delay(5);
+      odrive_serial << "t 0 " << move_to << '\n';
+      odrive_serial << "t 1 " << turns << '\n';
 
-        Serial.println("presiona 0 si quieres hacer un ensayo de tracción y 1 si quieres uno de twist: ");
+      Serial.println("presiona 0 si quieres hacer un ensayo de tracción y 1 si quieres uno de twist: ");
+      state = 5;
+      break;
+    case 5:
+      odrive_serial << "w axis0.controller.config.input_mode = INPUT_MODE_TRAP_TRAJ" << '\n';
+      //7 float intensity = GetIntensity(0);
+      float velocity = GetVelocity(0);
+      float pos = GetPosition(0);
+      if (velocity <100 && pos < 100) {
+       if (velocity>-100 && pos > -100) {
+        Serial.print("vel:");
+        Serial.print(velocity);
+        Serial.print(", ");
+        // Serial.print("intens:");
+        // Serial.print(intensity);
+        //  Serial.print(", ");
+        Serial.print("pos:");
+        Serial.print(pos);
+        Serial.println();
+        delay(10);
+      }
+
+      }
+
+      if (Serial.available() > 0) {
         state = 0;
+      }
       break;
     default:
       break;
